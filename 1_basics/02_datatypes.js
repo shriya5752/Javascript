6f3026e7ba26d8a9efe9d1e879bb0e2236f52290
shriya5752 <shriyamoh1362@gmail.com>
2025-07-09 16:59:44 +0000

    conversion of datatypes

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log();
+
+//typeof null---object
+//typeof undefined ---undefined
---
+++ 1_basics/03_ConversionOperations.js
@@ -0,0 +1,15 @@
+// let score="13"
+// console.log(typeof score);-->string
+
+// let valueInScore=Number(score)
+// console.log(typeof valueInScore);--->number
+
+
+let score="13abcd"
+let valueInScore=Number(score)
+console.log(typeof valueInScore);
+console.log(valueInScore)
+
+// "33"=> 33
+// "33abs"=>NaN
+// true=>1;  false=>0;
