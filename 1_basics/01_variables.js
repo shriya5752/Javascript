f6e25b7f2f93f9b8a3276e5044c78a5252857fee
shriya5752 <shriyamoh1362@gmail.com>
2025-07-11 12:01:16 +0000

    variables

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=11232
+let accountEmail="hi13@gmail.com"
+var accountPassword="12345"
+accountCity="bangalore" //not good
+let accountState
+
+//accountId=2
+
+accountEmail="lala.1362@gmail.com"
+accountPassword="23456"
+accountCity="delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var--
+issue in block scope and functional scope
+
+*/
