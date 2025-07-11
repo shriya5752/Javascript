55b60890153ed710f3a00fb2ad607a0a33f05dd8
shriya5752 <shriyamoh1362@gmail.com>
2025-07-11 11:59:31 +0000

    strings

--- 1_basics/03_ConversionOperations.js
+++ 1_basics/03_ConversionOperations.js
@@ -5,11 +5,29 @@
 // console.log(typeof valueInScore);--->number
 
 
-let score="13abcd"
-let valueInScore=Number(score)
-console.log(typeof valueInScore);
-console.log(valueInScore)
+// let score="13abcd"
+// let valueInScore=Number(score)
+// console.log(typeof valueInScore);
+// console.log(valueInScore)
 
+//when converting datatypes should start with caps
 // "33"=> 33
 // "33abs"=>NaN
 // true=>1;  false=>0;
+
+ 
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//=>122
+console.log(1+2+"2");//=>32
+
+console.log(3 + 4 * 5 % 3);
+
+
+
---
+++ 1_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name="Shriya"
+const age=18
+const newString="    hello  there hey    "
+
+console.log(`Heelo I'm ${name} and i'm ${age} years old`)
+
+console.log(newString.trim());
+console.log(newString.replace('hey','hi'))
+console.log(newString.includes('lala'))
+
+
+
